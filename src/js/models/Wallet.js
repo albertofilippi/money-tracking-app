32c3a8b9b74ea6ad8fa4cb13c38f460c88e131e6
afilippi <a.filippi@graphimeccgroup.it>
2023-03-06 12:07:17 +0100

    Create addOperation flow. Add saveWallet to store the wallet in the storage

--- src/js/models/Wallet.js
+++ src/js/models/Wallet.js
@@ -1,3 +1,8 @@
+var OpType = {
+  OUT: 0,
+  IN: 1,
+};
+
 function getWallet() {
   var wallet = localStorage.getItem("wallet");
   if (!wallet) {
@@ -7,6 +12,10 @@ function getWallet() {
     };
   }
   return JSON.parse(wallet);
+}
+
+function saveWallet(wallet) {
+  localStorage.setItem("wallet", JSON.stringify(wallet));
 }
 
 function Wallet() {
@@ -19,7 +28,24 @@ function Wallet() {
     operations = wallet.operations;
   }
 
-  this.addOperation = function () {};
+  this.addOperation = function (op) {
+    var operation = {
+      amount: op.amount,
+      description: op.description,
+      type: op.type,
+      date: new Date().getTime(),
+    };
+
+    if (op.type === OpType.IN) {
+      balance += operation.amount;
+    } else if (op.type === OpType.OUT) {
+      balance -= operation.amount;
+    }
+
+    operations.push(operation);
+
+    saveWallet();
+  };
 
   this.removeOperation = function () {};
 
@@ -32,4 +58,6 @@ function Wallet() {
   this.getOperations = function () {
     return operations;
   };
+
+  init();
 }
