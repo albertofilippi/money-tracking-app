c7f204bcc797ba14ec1414109ca39dd3201f29c2
afilippi <a.filippi@graphimeccgroup.it>
2023-03-06 11:59:07 +0100

    Adding getWallet function. Adding a new init function.

--- src/js/models/Wallet.js
+++ src/js/models/Wallet.js
@@ -1,6 +1,23 @@
+function getWallet() {
+  var wallet = localStorage.getItem("wallet");
+  if (!wallet) {
+    return {
+      balance: 0,
+      operations: [],
+    };
+  }
+  return JSON.parse(wallet);
+}
+
 function Wallet() {
   var balance = 0;
   var operations = [];
+
+  function init() {
+    var wallet = getWallet();
+    balance = wallet.balance;
+    operations = wallet.operations;
+  }
 
   this.addOperation = function () {};
 
